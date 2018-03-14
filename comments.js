d17c050e1bf204f9229586f42be7201c3210541b
Michael Gacek <gacek.michael@outlook.com>
2018-03-14 19:31:13 +0100

    Update comments.js

--- comments.js
+++ comments.js
@@ -4,22 +4,14 @@ import {EDIT_COMMENT} from './actions';
 import {THUMB_UP_COMMENT} from './actions';
 import {THUMB_DOWN_COMMENT} from './actions';
 
-const initialState = {
-    comments: [],
-    users: []
-};
-
 export const comments = (state = [], action) => {
     switch (action.type) {
         case ADD_COMMENT:
             return [{
                 id: action.id,
                 text: action.text,
-                votesUp: 0,
-                votesDown: 0
-            }, ...state.comments];
-            break;
-
+                votes: 0
+            }, ...state];
         case EDIT_COMMENT:
             const check = state.comments.find(comment => (comment.id === action.id));
             if (check) {
@@ -35,19 +27,13 @@ export const comments = (state = [], action) => {
             break;
 
         case THUMB_UP_COMMENT:
-            const check = state.comments.find(comment => (comment.id === action.id));
-            if (check) {
-                const index = state.comments.indexOf(check);
-                return [
-                   ...state.comments.slice(0, index),
-                   {...state.comments[index], votesUp: state.comments[index].votesUp + 1},
-                   ...state.comments.slice(index+1, state.comments.length)
-                ];
-            } else {
-                return state;
-            }
-            break;
-
+            return state.map(comment => {
+                if (comment.id === action.id) {
+                    return { ...state, votes: comment.votes + 1 };
+                }
+                
+                return comment;
+            });                     
         case THUMB_DOWN_COMMENT:
             const check = state.comments.find(comment => (comment.id === action.id));
             if (check) {
